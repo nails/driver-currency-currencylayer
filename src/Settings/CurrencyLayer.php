ead3eaa732434912f60bf3e40789c81e21b14845
Pablo de la Peña <p@shedcollective.org>
2021-03-25 13:58:13 +0000

    Replace settings with unified settings

---
+++ src/Settings/CurrencyLayer.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Nails\Currency\Driver\Settings;
+
+use Nails\Common\Helper\Form;
+use Nails\Common\Interfaces;
+use Nails\Common\Service\FormValidation;
+use Nails\Components\Setting;
+use Nails\Factory;
+
+/**
+ * Class CurrencyLayer
+ *
+ * @package Nails\Currency\Driver\Settings
+ */
+class CurrencyLayer implements Interfaces\Component\Settings
+{
+    const KEY_ACCESS_KEY = 'sAccessKey';
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * @inheritDoc
+     */
+    public function getLabel(): string
+    {
+        return 'Open Exchange Rates';
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * @inheritDoc
+     */
+    public function get(): array
+    {
+        /** @var Setting $oAccessKey */
+        $oAccessKey = Factory::factory('ComponentSetting');
+        $oAccessKey
+            ->setKey(static::KEY_ACCESS_KEY)
+            ->setLabel('Access Key')
+            ->setInfo('A paid account is required for currency conversions to work correctly')
+            ->setValidation([
+                FormValidation::RULE_REQUIRED,
+            ]);
+
+        return [
+            $oAccessKey,
+        ];
+    }
+}
